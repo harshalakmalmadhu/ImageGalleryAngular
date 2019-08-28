f290b2a754b4c8b200431d45300d59208f7fcd1a
harshalakmalmadhu <harshalakmal.hm@gmail.com>
2019-08-29 01:04:50 +0530

    add Firebase Upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { AngularFireModule } from "@angular/fire";
 import { AngularFireStorageModule} from "@angular/fire/storage";
 import { AngularFireDatabaseModule} from "@angular/fire/database";
+import { ReactiveFormsModule } from "@angular/forms";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -24,7 +25,8 @@ import { environment } from 'src/environments/environment';
     AppRoutingModule,
     AngularFireModule.initializeApp(environment.firebaseConfig),
     AngularFireStorageModule,
-    AngularFireDatabaseModule
+    AngularFireDatabaseModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/images/image/image.component.ts
+++ src/app/images/image/image.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { AngularFireStorage } from '@angular/fire/storage';
+import {finalize} from "rxjs/operators";
 
 @Component({
   selector: 'app-image',
@@ -7,9 +10,68 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ImageComponent implements OnInit {
 
-  constructor() { }
+  imageSrc : String; 
+  selectedImage: any ;
+  isSubmitted:Boolean;
+
+  formTemplate = new FormGroup({
+    caption : new FormControl('',Validators.required),
+    category : new FormControl(''),
+    imageUrl : new FormControl('',Validators.required)
+  })
+
+  constructor(private storage:AngularFireStorage) { }
 
   ngOnInit() {
+    this.resetForm();
+  }
+
+  showPreview(event:any){
+    if(event.target.files && event.target.files[0]){
+      const reader = new FileReader();
+      reader.onload = (e:any)=>this.imageSrc = e.target.result;
+      reader.readAsDataURL(event.target.files[0]);
+      this.selectedImage = event.target.files[0];
+    }
+    else{
+      this.imageSrc = '/assets/img/upload.jpeg';
+      this.selectedImage = null;
+      
+    }
+  }
+  onSubmit(FormValue){
+    this.isSubmitted=true;
+    if(this.formTemplate.valid){
+      var filePath = `${FormValue.category}/${this.selectedImage.name}_${new Date().getTime()}`;
+      const fileRef = this.storage.ref(filePath)
+      this.storage.upload(filePath,this.selectedImage).snapshotChanges().pipe(
+        finalize(()=>{
+          fileRef.getDownloadURL().subscribe((url)=>{
+
+            FormValue['imageUrl']=url;
+            this.resetForm();
+
+          })
+        })
+      
+      ).subscribe();
+    }
+
+  }
+  get formControls(){
+    return this.formTemplate['controls'];
+  }
+  resetForm(){
+    this.formTemplate.reset();
+    this.formTemplate.setValue({
+      caption:'',
+      imageUrl:'',
+      category:'Animal'
+
+    });
+    this.imageSrc='/assets/img/upload.jpeg';
+    this.isSubmitted = false;
+    this.selectedImage = null;
   }
 
 }
